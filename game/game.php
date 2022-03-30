ade62bfc6840b51d393037c487814623f19b582f
Juha-Pekka Lehtiö <juha.pekka.lehtio@gmail.com>
2022-03-30 20:44:39 +0300

    Basic layout for text card with four sides was fixed and the card is now drawn correctly.

--- game/game.php
+++ game/game.php
@@ -1,25 +1,24 @@
 <script src="playcardstogether.js"></script>
 
-<div class="card" align="center">
+<div class="card">
 	<div id="cardtop">
 		<div class="cardtextarea">TOP</div>
 	</div>
-	
-	<div class="cardmiddle">
+	<div id="cardmiddle" class="cardmiddle">
 		<div id="cardleft">
-			<div class="cardtextarea">LEFT</div>
+			<div id="cardleftcontent" class="cardtextarea">LEFT</div>
 		</div>
 		<div id="cardright">
-			<div class="cardtextarea">RIGHT</div>
+			<div id="cardrightcontent" class="cardtextarea">RIGHT</div>
 		</div>
 	</div>
-	<div id="cardbottom">
+		<div id="cardbottom">
 		<div class="cardtextarea">BOTTOM</div>
 	</div>
-
 </div>
 
 <script>
-	initializeCard();
-	setCardContent("Card title", "Card content that should be long enough to wrap to several lines!");
+initializeCard();
+setCardContent("Card title", "Card content that should be long enough to wrap to several lines!");
+setCardStyleDimensions();
 </script>
--- public/playcardstogether.js
+++ public/playcardstogether.js
@@ -1,11 +1,29 @@
-/**
- * 
- */
+
+function setCardStyleDimensions() {
+	var middleAreaWidth = document.getElementById("cardmiddle").offsetWidth;
+	var middleAreaHeight = document.getElementById("cardmiddle").offsetHeight;
+
+	var cardleft = document.getElementById("cardleft");
+	var cardright = document.getElementById("cardright");
+
+	cardleft.style.width = middleAreaHeight + "px";
+	cardright.style.width = middleAreaHeight + "px";
+
+	var cardleftHeight = document.getElementById("cardleft").offsetHeight;
+	var cardleftWidth = document.getElementById("cardleft").offsetWidth;
+
+	cardleft.style.top = middleAreaHeight / 2 - cardleftHeight / 2 + "px";
+	cardright.style.top = middleAreaHeight / 2 - cardleftHeight / 2 + "px";
+
+	cardleft.style.left = cardleftHeight / 2 - cardleftWidth / 2 + "px";
+	cardright.style.right = cardleftHeight / 2 - cardleftWidth / 2 + "px";
+
+}
 
 function initializeCard(){
 	var cardElements = document.getElementsByClassName("cardtextarea");
 	for(var i = 0; i < cardElements.length; i++){
-		cardElements[i].innerHTML = "<h1 class='cardtitle'></h1><p class='carddescription'></p>";
+		cardElements[i].innerHTML = "<div class='cardtitle'></div><div class='carddescription'></div>";
 	}
 }
 
